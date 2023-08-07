ce2a0cfdc509cd971c1a7f49183c1d9c2cbc08c1
chandeo37 <chandeo@gmail.com>
2023-08-07 16:55:29 -0400

    Practica Consolidacion 7

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="HomeView">
+
+    <ListaComponent titulo="Lista de Cursos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListaComponent from '@/components/ListaComponent.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    ListaComponent
+  }
+}
+</script>
+
+<style scoped>
+#HomeView{
+  background-color:lightskyblue;
+}
+</style>
